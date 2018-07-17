94592b6dc7fb03c94100de93334b5810aefebb64
sys-user <gsivabe@yahoo.co.in>
2018-07-17 11:55:27 +0530

    my first commit

---
+++ Intro.py
@@ -0,0 +1,20 @@
+#Keywords in Python
+import keyword
+print(keyword.kwlist)
+tt=len(keyword.kwlist)
+print ("Total number of keywords is %d " % (tt))
+print("\n\n")
+
+#Example for Input
+print("BELLOW ARE EXAMPLE OF INPUT STATEMENT")
+num2 = int(input('Please enter a number: '))
+print('User has entered %d as input' % (num2))
+print("\n\n")
+
+#Example for Print
+print("BELLOW ARE EXAMPLES OF PRINT STATEMENT")
+print('5'*6)
+print(5,6,7)
+print('Python', 1, 2)
+
+
